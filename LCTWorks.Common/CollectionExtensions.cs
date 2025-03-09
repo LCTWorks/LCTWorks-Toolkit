b61acfcfeb90cdf266dc59acd5ea3399580dc509
Leisvan Cordero <rathma@live.com>
2025-03-09 12:17:49 -0600

    Add ToKeyValuePairs extension method to CollectionExtensions

--- LCTWorks.Common/CollectionExtensions.cs
+++ LCTWorks.Common/CollectionExtensions.cs
@@ -1,25 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace LCTWorks.Common;
 
-namespace LCTWorks.Common
+public static class CollectionExtensions
 {
-    public static class CollectionExtensions
+    public static IEnumerable<KeyValuePair<TKey, TValue>> ToKeyValuePairs<TKey, TValue>(this IEnumerable<(TKey Key, TValue Value)> items)
     {
-        public static IEnumerable<KeyValuePair<string, string>> ToValidStringKeyValuePair(this IEnumerable<(string Key, string Value)> items)
+        if (items == null)
         {
-            if (items == null)
-            {
-                return [];
-            }
-            return items.Select(x =>
-            {
-                var validKey = string.IsNullOrEmpty(x.Key) ? Guid.NewGuid().ToString() : x.Key;
-                var validVal = string.IsNullOrEmpty(x.Value) ? "-" : x.Value;
-                return new KeyValuePair<string, string>(validKey, validVal);
-            });
+            return [];
         }
+        return items.Select(x => new KeyValuePair<TKey, TValue>(x.Key, x.Value));
+    }
+
+    public static IEnumerable<KeyValuePair<string, string>> ToValidStringKeyValuePair(this IEnumerable<(string Key, string Value)> items)
+    {
+        if (items == null)
+        {
+            return [];
+        }
+        return items.Select(x =>
+        {
+            var validKey = string.IsNullOrEmpty(x.Key) ? Guid.NewGuid().ToString() : x.Key;
+            var validVal = string.IsNullOrEmpty(x.Value) ? "-" : x.Value;
+            return new KeyValuePair<string, string>(validKey, validVal);
+        });
     }
 }
