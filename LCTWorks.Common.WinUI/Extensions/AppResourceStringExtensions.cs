108b8f8f76769848e9f0caa5002e88aa26f953ae
Leisvan Cordero <rathma@live.com>
2025-06-21 15:10:19 -0600

    Add AppCenter as an extra telemetry layer.

---
+++ LCTWorks.Common.WinUI/Extensions/AppResourceStringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace LCTWorks.Common.WinUI.Extensions
+{
+    public static class AppResourceStringExtensions
+    {
+        public static T? GetAppResource<T>(this string key)
+        {
+            return Application.Current.Resources.ContainsKey(key)
+                ? (T)Application.Current.Resources[key]
+                : default;
+        }
+    }
+}
