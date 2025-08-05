41b92c64978b1ada2e29d4355c2a74a3e6d2ccdc
Leisvan Cordero <rathma@live.com>
2025-08-05 11:15:00 -0600

    Add ChipControl

---
+++ LCTWorks.Common.WinUI/Controls/ChipControl.cs
@@ -0,0 +1,25 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace LCTWorks.Common.WinUI.Controls;
+
+public sealed partial class ChipControl : Control
+{
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register(
+        nameof(Text),
+        typeof(string),
+        typeof(ChipControl),
+        new PropertyMetadata(string.Empty));
+
+    public ChipControl()
+    {
+        DefaultStyleKey = typeof(ChipControl);
+    }
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+}
