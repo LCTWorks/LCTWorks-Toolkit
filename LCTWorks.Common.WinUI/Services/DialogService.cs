fd5d9c9028accc066339c1d4fc586405fdee7e7d
Leisvan Cordero <leisvanc@kortext.com>
2025-08-18 10:49:30 -0600

    Add DialogService

---
+++ LCTWorks.Common.WinUI/Services/DialogService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCTWorks.Common.WinUI.Services
+{
+    public class DialogService
+    {
+        private ContentDialog? _currentContentDialog;
+        private bool _suppressEscKey;
+
+        public ElementTheme Theme { get; set; } = ElementTheme.Default;
+
+        public XamlRoot? XamlRoot { get; set; }
+
+        public void HideCurrentContentDialog()
+        {
+            _suppressEscKey = false;
+            _currentContentDialog?.Hide();
+        }
+
+        public Task<ContentDialogResult> ShowDialogAsync(ContentDialog dialog)
+            => RegisterAndShowAsync(dialog);
+
+        private void OnCurrentContentDialogClosing(ContentDialog sender, ContentDialogClosingEventArgs args)
+        {
+            if (args.Result == ContentDialogResult.None && _suppressEscKey)
+            {
+                args.Cancel = true;
+            }
+            else
+            {
+                sender.Closing -= OnCurrentContentDialogClosing;
+                _currentContentDialog = null;
+                _suppressEscKey = false;
+            }
+        }
+
+        private async Task<ContentDialogResult> RegisterAndShowAsync(ContentDialog dialog, bool suppressEscKey = false)
+        {
+            HideCurrentContentDialog();
+
+            if (XamlRoot == null)
+            {
+                return ContentDialogResult.None;
+            }
+
+            _suppressEscKey = suppressEscKey;
+            _currentContentDialog = dialog;
+            _currentContentDialog.XamlRoot = XamlRoot;
+            _currentContentDialog.Closing += OnCurrentContentDialogClosing;
+            _currentContentDialog.RequestedTheme = Theme;
+
+            return await _currentContentDialog.ShowAsync();
+        }
+    }
+}
