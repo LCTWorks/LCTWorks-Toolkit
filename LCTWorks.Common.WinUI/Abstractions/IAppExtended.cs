07df073a5295327d0897adcc127f882ceec7499d
Leisvan Cordero <rathma@live.com>
2025-08-30 15:56:01 -0600

    Add TitleBarHelper

---
+++ LCTWorks.Common.WinUI/Abstractions/IAppExtended.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace LCTWorks.Common.WinUI.Abstractions;
+
+public interface IAppExtended
+{
+    UIElement? AppTitleBar
+    {
+        get; set;
+    }
+
+    Window MainWindow
+    {
+        get;
+    }
+}
