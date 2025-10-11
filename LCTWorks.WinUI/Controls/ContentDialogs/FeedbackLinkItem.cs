c427aec05ef5f694c35261c3c7c35e145151565a
Leisvan Cordero <rathma@live.com>
2025-10-11 12:44:12 -0600

    Refactor FeedbackLinkItem and centralize resources
    Refactored the `FeedbackLinkItem` class to remove the `Url` property and `OpenLink` command, introducing `Command` and `CommandParameter` for better flexibility. Updated `ItemsViewContentDialog.xaml` to use a `FeedbackLinkItemDataTemplateSelector` and replaced `ItemsRepeater` with `ListView` for dynamic template selection.
    
    Centralized layout constants, font families, and text styles into separate resource dictionaries (`Constants.xaml`, `FontFamilies.xaml`, `TextBlocks.xaml`) for improved modularity and maintainability. Refactored `Generic.xaml` to reference these external resources.
    
    Added `FeedbackLinkItemDataTemplateSelector` to dynamically choose templates based on the presence of a `Command`. Enhanced UI flexibility and maintainability by simplifying redundant styles and improving resource organization.

--- LCTWorks.WinUI/Controls/ContentDialogs/FeedbackLinkItem.cs
+++ LCTWorks.WinUI/Controls/ContentDialogs/FeedbackLinkItem.cs
@@ -1,29 +1,19 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
+using System.Windows.Input;
 
 namespace LCTWorks.WinUI.Controls.ContentDialogs;
 
-public partial class FeedbackLinkItem(string title, string description, string iconUri, string url) : ObservableObject
+public partial class FeedbackLinkItem(string title, string description, string iconUri) : ObservableObject
 {
+    public ICommand? Command { get; set; }
+
+    public object? CommandParameter { get; set; }
+
     public string Description { get; } = description;
 
     public string IconUri { get; } = iconUri;
 
     public string Title { get; } = title;
-
-    public string Url { get; } = url;
-
-    [RelayCommand]
-    private void OpenLink()
-    {
-        try
-        {
-            var uri = new Uri(Url);
-            _ = Windows.System.Launcher.LaunchUriAsync(uri);
-        }
-        catch
-        {
-        }
-    }
 }
---
+++ LCTWorks.WinUI/Controls/Internal/FeedbackLinkItemDataTemplateSelector.cs
@@ -0,0 +1,25 @@
+﻿using LCTWorks.WinUI.Controls.ContentDialogs;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace LCTWorks.WinUI.Controls.Internal;
+
+internal partial class FeedbackLinkItemDataTemplateSelector : DataTemplateSelector
+{
+    public FeedbackLinkItemDataTemplateSelector()
+    {
+    }
+
+    public DataTemplate? InteractiveTemplate { get; set; }
+
+    public DataTemplate? NonInteractiveTemplate { get; set; }
+
+    protected override DataTemplate SelectTemplateCore(object item)
+    {
+        if (item is FeedbackLinkItem linkItem)
+        {
+            return linkItem.Command is not null ? InteractiveTemplate ?? base.SelectTemplateCore(item) : NonInteractiveTemplate ?? base.SelectTemplateCore(item);
+        }
+        return base.SelectTemplateCore(item);
+    }
+}
