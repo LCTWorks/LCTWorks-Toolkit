e133ca6adea8f2cfce5141163112d76005d5f0f4
Leisvan Cordero <rathma@live.com>
2025-10-29 11:55:28 -0600

    Add ConsoleLogger

---
+++ LCTWorks.Services/Logging/ConsoleLoggerProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace LCTWorks.Services.Logging;
+
+public sealed class ConsoleLoggerProvider : ILoggerProvider
+{
+    public ILogger CreateLogger(string categoryName)
+    {
+        return new ConsoleLogger(categoryName);
+    }
+
+    public void Dispose()
+    { }
+}
