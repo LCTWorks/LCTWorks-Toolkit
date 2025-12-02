2471d30cadfdf08fef5a2d3a2859a5d3004a92b5
Leisvan Cordero <rathma@live.com>
2025-12-02 15:50:41 -0600

    Add custom NavigationViewItemButton control

---
+++ LCTWorks.WinUI/Controls/NavigationViewItemButton.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace LCTWorks.WinUI.Controls;
+
+public sealed partial class NavigationViewItemButton : Button
+{
+    public NavigationViewItemButton()
+    {
+        DefaultStyleKey = typeof(NavigationViewItemButton);
+    }
+
+    public IconElement Icon
+    {
+        get => (IconElement)GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register(nameof(Icon), typeof(IconElement), typeof(NavigationViewItemButton),
+            new PropertyMetadata(default));
+}
