3ca1d80c26553de0b8f7ee8eacbd833370fc3567
Leisvan Cordero <rathma@live.com>
2025-12-02 17:14:30 -0600

    Refactor NavigationViewItemButton for custom content

--- LCTWorks.WinUI/Controls/NavigationViewItemButton.xaml.cs
+++ LCTWorks.WinUI/Controls/NavigationViewItemButton.xaml.cs
@@ -5,18 +5,52 @@ namespace LCTWorks.WinUI.Controls;
 
 public sealed partial class NavigationViewItemButton : Button
 {
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register(nameof(Icon), typeof(object), typeof(NavigationViewItemButton),
+            new PropertyMetadata(null, OnIconChanged));
+
+    public static readonly DependencyProperty CustomContentProperty =
+        DependencyProperty.Register(nameof(CustomContent), typeof(object), typeof(NavigationViewItemButton),
+            new PropertyMetadata(null, OnCustomContentChanged));
+
+    public object Icon
+    {
+        get => (object)GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+
+    public object CustomContent
+    {
+        get => (object)GetValue(CustomContentProperty);
+        set => SetValue(CustomContentProperty, value);
+    }
+
     public NavigationViewItemButton()
     {
         DefaultStyleKey = typeof(NavigationViewItemButton);
     }
 
-    public IconElement Icon
+    protected override void OnApplyTemplate()
     {
-        get => (IconElement)GetValue(IconProperty);
-        set => SetValue(IconProperty, value);
+        base.OnApplyTemplate();
+        UpdateVisualStates(useTransitions: false);
     }
 
-    public static readonly DependencyProperty IconProperty =
-        DependencyProperty.Register(nameof(Icon), typeof(IconElement), typeof(NavigationViewItemButton),
-            new PropertyMetadata(default));
+    private static void OnIconChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (NavigationViewItemButton)d;
+        control.UpdateVisualStates(useTransitions: true);
+    }
+
+    private static void OnCustomContentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var control = (NavigationViewItemButton)d;
+        control.UpdateVisualStates(useTransitions: true);
+    }
+
+    private void UpdateVisualStates(bool useTransitions)
+    {
+        VisualStateManager.GoToState(this, Icon is null ? "IconHidden" : "IconVisible", useTransitions);
+        VisualStateManager.GoToState(this, CustomContent is null ? "CustomContentHidden" : "CustomContentVisible", useTransitions);
+    }
 }
