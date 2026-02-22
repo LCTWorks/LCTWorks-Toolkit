31e6a3f46a54e429a5ef3453389a76a7da6cce17
Leisvan Cordero <rathma@live.com>
2026-02-21 21:43:24 -0600

    Update NuGet packaging and enable WebView2 extensions

--- LCTWorks.WinUI/Extensions/WebView2Extensions.cs
+++ LCTWorks.WinUI/Extensions/WebView2Extensions.cs
@@ -5,51 +5,51 @@ using System.Threading.Tasks;
 
 namespace LCTWorks.WinUI.Extensions;
 
-//public static class WebView2Extensions
-//{
-//    public static async Task<string?> GetHtmlAsync(this WebView2 webView2)
-//    {
-//        if (webView2 == null)
-//        {
-//            return default;
-//        }
-//        if (webView2.CoreWebView2 == null)
-//        {
-//            await webView2.EnsureCoreWebView2Async();
-//        }
-//        var result = await webView2.CoreWebView2?.ExecuteScriptAsync("document.documentElement.outerHTML;");
-//        if (!string.IsNullOrEmpty(result))
-//        {
-//            return Json.ToObject<string>(result);
-//        }
-//        return default;
-//    }
+public static class WebView2Extensions
+{
+    public static async Task<string?> GetHtmlAsync(this WebView2 webView2)
+    {
+        if (webView2 == null)
+        {
+            return default;
+        }
+        if (webView2.CoreWebView2 == null)
+        {
+            await webView2.EnsureCoreWebView2Async();
+        }
+        var result = await webView2.CoreWebView2?.ExecuteScriptAsync("document.documentElement.outerHTML;");
+        if (!string.IsNullOrEmpty(result))
+        {
+            return Json.ToObject<string>(result);
+        }
+        return default;
+    }
 
-//    public static async Task<string?> GetInnerTextAsync(this WebView2 webView2)
-//    {
-//        if (webView2 == null)
-//        {
-//            return default;
-//        }
-//        if (webView2.CoreWebView2 == null)
-//        {
-//            await webView2.EnsureCoreWebView2Async();
-//        }
-//        var result = await webView2.CoreWebView2?.ExecuteScriptAsync("document.body.innerText");
-//        if (!string.IsNullOrEmpty(result))
-//        {
-//            return Json.ToObject<string>(result);
-//        }
-//        return default;
-//    }
+    public static async Task<string?> GetInnerTextAsync(this WebView2 webView2)
+    {
+        if (webView2 == null)
+        {
+            return default;
+        }
+        if (webView2.CoreWebView2 == null)
+        {
+            await webView2.EnsureCoreWebView2Async();
+        }
+        var result = await webView2.CoreWebView2?.ExecuteScriptAsync("document.body.innerText");
+        if (!string.IsNullOrEmpty(result))
+        {
+            return Json.ToObject<string>(result);
+        }
+        return default;
+    }
 
-//    public static bool HasCertificateErrors(this CoreWebView2NavigationCompletedEventArgs args)
-//    {
-//        var errorStatus = args.WebErrorStatus;
-//        return errorStatus == CoreWebView2WebErrorStatus.CertificateCommonNameIsIncorrect ||
-//             errorStatus == CoreWebView2WebErrorStatus.ClientCertificateContainsErrors ||
-//             errorStatus == CoreWebView2WebErrorStatus.CertificateRevoked ||
-//             errorStatus == CoreWebView2WebErrorStatus.CertificateIsInvalid ||
-//             errorStatus == CoreWebView2WebErrorStatus.ServerUnreachable;
-//    }
-//}
+    public static bool HasCertificateErrors(this CoreWebView2NavigationCompletedEventArgs args)
+    {
+        var errorStatus = args.WebErrorStatus;
+        return errorStatus == CoreWebView2WebErrorStatus.CertificateCommonNameIsIncorrect ||
+             errorStatus == CoreWebView2WebErrorStatus.ClientCertificateContainsErrors ||
+             errorStatus == CoreWebView2WebErrorStatus.CertificateRevoked ||
+             errorStatus == CoreWebView2WebErrorStatus.CertificateIsInvalid ||
+             errorStatus == CoreWebView2WebErrorStatus.ServerUnreachable;
+    }
+}
