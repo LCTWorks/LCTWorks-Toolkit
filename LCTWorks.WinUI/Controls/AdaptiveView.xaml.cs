101958115e097752a053a762b48544f2217830a9
Leisvan Cordero <rathma@live.com>
2026-03-29 13:52:04 -0600

    Add soft animation options

--- LCTWorks.WinUI/Controls/AdaptiveView.xaml.cs
+++ LCTWorks.WinUI/Controls/AdaptiveView.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using LCTWorks.Core.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Hosting;
 using Microsoft.UI.Xaml.Input;
 using System;
+using System.Numerics;
 using System.Threading.Tasks;
 
 namespace LCTWorks.WinUI.Controls;
@@ -28,9 +30,17 @@ public partial class AdaptiveView : Control
             DependencyProperty.Register(nameof(Layout), typeof(Layout), typeof(AdaptiveView),
             new PropertyMetadata(default));
 
+    public static readonly DependencyProperty UseSoftAnimationsProperty =
+        DependencyProperty.Register(nameof(UseSoftAnimations), typeof(bool), typeof(AdaptiveView),
+            new PropertyMetadata(false));
+
     private const string ItemsRepeaterPartName = "ItemsRepeater";
     private const string ItemsScrollHostPartName = "ItemsScrollHost";
     private const string ItemsScrollPartName = "ItemsScroll";
+
+    private static readonly DependencyProperty SoftAnimationDataProperty =
+        DependencyProperty.RegisterAttached("SoftAnimationData", typeof(SoftAnimationData), typeof(AdaptiveView),
+            new PropertyMetadata(null));
 
     private ItemsRepeater? _itemsRepeater;
     private ScrollViewer? _itemsScroll;
@@ -66,6 +76,12 @@ public partial class AdaptiveView : Control
     {
         get => (Layout)GetValue(LayoutProperty);
         set => SetValue(LayoutProperty, value);
+    }
+
+    public bool UseSoftAnimations
+    {
+        get => (bool)GetValue(UseSoftAnimationsProperty);
+        set => SetValue(UseSoftAnimationsProperty, value);
     }
 
     public async void BringIntoView(object item)
@@ -121,18 +137,89 @@ public partial class AdaptiveView : Control
     {
         base.OnApplyTemplate();
 
-        _itemsRepeater?.Tapped -= OnItemsRepeaterTapped;
+        if (_itemsRepeater is not null)
+        {
+            _itemsRepeater.Tapped -= OnItemsRepeaterTapped;
+            _itemsRepeater.ElementPrepared -= OnElementPrepared;
+            _itemsRepeater.ElementClearing -= OnElementClearing;
+        }
 
         _itemsRepeater = GetTemplateChild(ItemsRepeaterPartName) as ItemsRepeater;
         _itemsScroll = GetTemplateChild(ItemsScrollPartName) as ScrollViewer;
         _itemsScrollHost = GetTemplateChild(ItemsScrollHostPartName) as ItemsRepeaterScrollHost;
 
-        _itemsRepeater?.Tapped += OnItemsRepeaterTapped;
+        if (_itemsRepeater is not null)
+        {
+            _itemsRepeater.Tapped += OnItemsRepeaterTapped;
+            _itemsRepeater.ElementPrepared += OnElementPrepared;
+            _itemsRepeater.ElementClearing += OnElementClearing;
+        }
     }
 
     protected virtual void OnItemClicked(AdaptiveViewItemClickedEventArgs args)
     {
         ItemClicked?.Invoke(this, args);
+    }
+
+    private static void AttachSoftAnimations(UIElement element)
+    {
+        ElementCompositionPreview.SetIsTranslationEnabled(element, true);
+
+        var visual = ElementCompositionPreview.GetElementVisual(element);
+        var compositor = visual.Compositor;
+
+        var offsetUp = compositor.CreateVector3KeyFrameAnimation();
+        offsetUp.InsertKeyFrame(1.0f, new Vector3(0, -4, 0));
+
+        var offsetDown = compositor.CreateVector3KeyFrameAnimation();
+        offsetDown.InsertKeyFrame(1.0f, new Vector3(0, 0, 0));
+
+        var scale = compositor.CreateVector3KeyFrameAnimation();
+        scale.InsertKeyFrame(0f, new Vector3(0.96f));
+        scale.InsertKeyFrame(1f, new Vector3(1f));
+        scale.Duration = TimeSpan.FromMilliseconds(800);
+
+        element.SetValue(SoftAnimationDataProperty, new SoftAnimationData(visual, offsetUp, offsetDown, scale));
+
+        element.PointerEntered += OnItemPointerEntered;
+        element.PointerExited += OnItemPointerExited;
+        element.Tapped += OnItemTapped;
+    }
+
+    private static void DetachSoftAnimations(UIElement element)
+    {
+        element.SetValue(SoftAnimationDataProperty, null);
+        element.PointerEntered -= OnItemPointerEntered;
+        element.PointerExited -= OnItemPointerExited;
+        element.Tapped -= OnItemTapped;
+    }
+
+    private static SoftAnimationData? GetAnimationData(UIElement element) =>
+        element.GetValue(SoftAnimationDataProperty) as SoftAnimationData;
+
+    private static void OnItemPointerEntered(object sender, PointerRoutedEventArgs e)
+    {
+        if (GetAnimationData((UIElement)sender) is { } data)
+        {
+            data.Visual.StartAnimation("Translation", data.OffsetUp);
+        }
+    }
+
+    private static void OnItemPointerExited(object sender, PointerRoutedEventArgs e)
+    {
+        if (GetAnimationData((UIElement)sender) is { } data)
+        {
+            data.Visual.StartAnimation("Translation", data.OffsetDown);
+        }
+    }
+
+    private static void OnItemTapped(object sender, TappedRoutedEventArgs e)
+    {
+        if (GetAnimationData((UIElement)sender) is { } data)
+        {
+            data.Visual.CenterPoint = new Vector3(data.Visual.Size / 2, 0);
+            data.Visual.StartAnimation("Scale", data.Scale);
+        }
     }
 
     private UIElement? FindRepeaterChildFromSource(DependencyObject source)
@@ -163,6 +250,27 @@ public partial class AdaptiveView : Control
         var items = new TolerantCollection(ItemsSource);
         return items.IndexOf(item);
     }
+
+    private void OnElementClearing(ItemsRepeater sender, ItemsRepeaterElementClearingEventArgs args)
+    {
+        DetachSoftAnimations(args.Element);
+    }
+
+    private void OnElementPrepared(ItemsRepeater sender, ItemsRepeaterElementPreparedEventArgs args)
+    {
+        if (!UseSoftAnimations)
+        {
+            return;
+        }
+
+        AttachSoftAnimations(args.Element);
+    }
+
+    private sealed record SoftAnimationData(
+        Microsoft.UI.Composition.Visual Visual,
+        Microsoft.UI.Composition.Vector3KeyFrameAnimation OffsetUp,
+        Microsoft.UI.Composition.Vector3KeyFrameAnimation OffsetDown,
+        Microsoft.UI.Composition.Vector3KeyFrameAnimation Scale);
 
     private void OnItemsRepeaterTapped(object sender, TappedRoutedEventArgs e)
     {
@@ -200,20 +308,4 @@ public partial class AdaptiveView : Control
             OnItemClicked(new AdaptiveViewItemClickedEventArgs(clickedItem, element, index));
         }
     }
-}
-
-public class AdaptiveViewItemClickedEventArgs : EventArgs
-{
-    public AdaptiveViewItemClickedEventArgs(object clickedItem, UIElement container, int index)
-    {
-        ClickedItem = clickedItem;
-        Container = container;
-        Index = index;
-    }
-
-    public object ClickedItem { get; }
-
-    public UIElement Container { get; }
-
-    public int Index { get; }
 }
