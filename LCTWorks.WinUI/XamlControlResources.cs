3876b84ad76703c12197f36f01cfbaa89fd43cfa
Leisvan Cordero <rathma@live.com>
2026-05-09 21:36:49 -0600

    Update resource merging and content inclusion logic

--- LCTWorks.WinUI/XamlControlResources.cs
+++ LCTWorks.WinUI/XamlControlResources.cs
@@ -7,6 +7,14 @@ public partial class XamlControlsResources : ResourceDictionary
 {
     public XamlControlsResources()
     {
-        Source = new Uri("ms-appx:///LCTWorks.WinUI/Themes/Generic.xaml", UriKind.Absolute);
+        MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///LCTWorks.WinUI/Themes/Constants.xaml", UriKind.Absolute) });
+        MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///LCTWorks.WinUI/Themes/FontFamilies.xaml", UriKind.Absolute) });
+        MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///LCTWorks.WinUI/Themes/TextBlocks.xaml", UriKind.Absolute) });
+        MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///LCTWorks.WinUI/Controls/Chip.xaml", UriKind.Absolute) });
+        MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///LCTWorks.WinUI/Controls/ThemedButton.xaml", UriKind.Absolute) });
+        MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///LCTWorks.WinUI/Controls/MenuFlyoutHeader.xaml", UriKind.Absolute) });
+        MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///LCTWorks.WinUI/Controls/NavigationViewItemButton.xaml", UriKind.Absolute) });
+        MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///LCTWorks.WinUI/Controls/AdaptiveImage.xaml", UriKind.Absolute) });
+        MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///LCTWorks.WinUI/Controls/AdaptiveView.xaml", UriKind.Absolute) });
     }
 }
