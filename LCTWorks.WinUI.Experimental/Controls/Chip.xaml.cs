1501ce4fc455a5b095f688ec7bbe53d62455303c
Leisvan Cordero <rathma@live.com>
2026-05-10 20:54:00 -0600

    More components working

---
+++ LCTWorks.WinUI.Experimental/Controls/Chip.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace LCTWorks.WinUI.Experimental.Controls;
+
+public sealed partial class Chip : Control
+{
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register(
+        nameof(Text),
+        typeof(string),
+        typeof(Chip),
+        new PropertyMetadata(string.Empty));
+
+    public Chip()
+    {
+        DefaultStyleKey = typeof(Chip);
+    }
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+}
---
+++ LCTWorks.WinUI.Experimental/Controls/MenuFlyoutHeader.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace LCTWorks.WinUI.Experimental.Controls;
+
+public sealed partial class MenuFlyoutHeader : MenuFlyoutSeparator
+{
+    public static readonly DependencyProperty HeaderProperty =
+        DependencyProperty.Register(
+        nameof(Header),
+        typeof(string),
+        typeof(MenuFlyoutHeader),
+        new PropertyMetadata(string.Empty));
+
+    public MenuFlyoutHeader()
+    {
+        DefaultStyleKey = typeof(MenuFlyoutHeader);
+    }
+
+    public string Header
+    {
+        get => (string)GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+}
