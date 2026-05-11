663e7cecb8d753b3d65381a0d45eb146015d06f0
Leisvan Cordero <rathma@live.com>
2026-05-11 11:34:02 -0600

    Fixes

--- LCTWorks.Workshop.Empty/MainWindow.xaml.cs
+++ LCTWorks.Workshop.Empty/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-using LCTWorks.WinUI.Experimental.Controls.ContentDialogs;
+using LCTWorks.WinUI.Controls.ContentDialogs;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
